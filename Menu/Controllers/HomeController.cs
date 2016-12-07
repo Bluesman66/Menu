c2acdaaae24973f44e5e7dc81342fa2ff3acd5da
alex.paclin <alex.paclin@gmail.com>
2016-12-08 00:35:42 +0500

    Управление иерархическими данными

--- Menu/Controllers/HomeController.cs
+++ Menu/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Menu.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,18 @@ namespace Menu.Controllers
 {
 	public class HomeController : Controller
 	{
+		ApplicationContext db = new ApplicationContext();
+
 		public ActionResult Index()
 		{
 			return View();
-		}		
+		}	
+
+		public ActionResult _Menu()
+		{
+			List<MenuItem> menuItems = db.MenuItems.ToList();
+
+			return PartialView("_Menu", menuItems);
+		}
 	}
 }
